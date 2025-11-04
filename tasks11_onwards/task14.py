b985fa6e39099dccd75257ecd24ecc09b0e5efc9
Profcode <imwangi31@gmail.com>
2025-11-04 12:31:02 +0300

    Try except

--- tasks11_onwards/task14.py
+++ tasks11_onwards/task14.py
@@ -1,2 +1,14 @@
 # Write a program that takes input of 2 values and adds them
-value_1 = input("Enter value: ")
+# valid_number = int or float
+
+while True:
+    try:
+        value_1 = float(input("Enter first number: "))
+        value_2 = float(input("Enter second number: "))
+        print(f"The sum is equal to {value_1 + value_2}")
+        break
+    except ValueError:
+        print("Invalid character")
+
+
+
