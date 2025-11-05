6c5fbc5a6de4e62c47b27337255f0d1d38ccbc0b
Profcode <imwangi31@gmail.com>
2025-11-05 11:40:58 +0300

    Functions

--- task1.py
+++ task1.py
@@ -1,5 +1,16 @@
 # A = 1/2 * b * h
-b = input("Enter base: ")
-h = input("Enter height: ")
-A = 1/2 * int(b) * int(h)
-print(A)
+# b = input("Enter base: ")
+# h = input("Enter height: ")
+# A = 1/2 * int(b) * int(h)
+# print(A)
+
+# handle using functions
+def triangle_area(b,h):
+    return(0.5 * b * h)
+
+b = float(input("Enter base: "))
+h = float(input("Enter height: "))
+
+area = triangle_area(b,h)
+print(area)
+
