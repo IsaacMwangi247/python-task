fc5f136b37e1d3b01905b6d707194806c489c3d7
Profcode <imwangi31@gmail.com>
2025-11-06 15:16:47 +0300

    Question 1

---
+++ tasks11_onwards/functions_task.py
@@ -0,0 +1,14 @@
+# Write a program to reverse a string
+
+string = input("Enter a string: ")
+def reverse_string(s):
+    return string[::-1]
+reversed =string[::-1]
+
+reverse_string(string)
+print(reversed)
+
+
+
+# write a program to print even numbers for a list
+
