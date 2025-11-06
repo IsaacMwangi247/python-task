3c7ed58f82b6b5f0f41813bcb9858ecc5213f598
Profcode <imwangi31@gmail.com>
2025-11-06 14:42:41 +0300

    Updates functions

---
+++ new_version1520.py
@@ -0,0 +1,53 @@
+# Write a program that takes input of someone's basic salary and benefits, 
+# adds them to find the gross salary 
+# then uses  the gross salary to find the NHIF rate
+
+basic_salary = float(input("Enter basic salary: "))
+benefits = float(input("Enter benefits: "))
+
+def get_salary(a,b):
+    return a + b
+
+
+def get_nhif(nhif):
+    if gross_salary <=5999:
+        nhif = "150"
+    elif gross_salary <=7999:
+        nhif = "300"
+    elif gross_salary <=11999:
+        nhif = "400"
+    elif gross_salary <=14999:
+        nhif = "500"
+    elif gross_salary <=19999:
+        nhif = "600"
+    elif gross_salary <=24999:
+        nhif = "750"
+    elif gross_salary <=29999:
+        nhif = "850"
+    elif gross_salary <=34999:
+        nhif = "900"
+    elif gross_salary <=39999:
+        nhif = "950"
+    elif gross_salary <=44999:
+        nhif = "1000"
+    elif gross_salary <=49999:
+        nhif = "1100"
+    elif gross_salary <=59999:
+        nhif = "1200"
+    elif gross_salary <=69999:
+        nhif = "1300"
+    elif gross_salary <=79999:
+        nhif = "1400"
+    elif gross_salary <=89999:
+        nhif = "1500"
+    elif gross_salary <=99999:
+        nhif = "1600"
+    elif gross_salary >=100000:
+        nhif = "1700"
+
+    print(f"NHIF Rate: {nhif}")
+
+
+gross_salary = get_salary(basic_salary,benefits)
+print(gross_salary)
+
---
+++ slide70_task.py
@@ -0,0 +1,40 @@
+# Write a python program that takes from a user 5 inputs (maths, eng, swa, sci, sos). 
+# Create a function that calculates the total marks another the average marks ,
+# then a function that finds the grade according to the table below. 
+# Use the value from total to get the average and average to find the grade.
+# A > 79 , B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40
+
+
+maths = float(input("Enter Math score: "))
+eng = float(input("Enter English score: "))
+swa = float(input("Enter Kiswahili score: "))
+sci = float(input("Enter Science score: "))
+sos = float(input("Enter SOS score: "))
+
+def get_total(a,b,c,d,e): 
+    return a + b + c + d + e
+
+def get_average(value):
+    return value / 5
+
+def get_grade(avg):
+    if avg > 70 and avg < 100:
+        return "A"
+    elif avg > 60 and avg < 79:
+        return "B"
+    elif avg > 50 and avg < 59:
+        return "C"
+    elif avg > 40 and avg < 49:
+        return "D"
+    elif avg < 40:
+        return "E"
+    else:
+        return "Invalid Mark"
+
+total_mark = get_total(maths,eng,swa,sci,sos)
+average = get_average(total_mark)
+grade = get_grade(average)
+
+print("Total mark is: ",total_mark)
+print("Average mark is: ",average)
+print("Final grade is: ",grade)
